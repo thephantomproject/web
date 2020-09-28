77a41613ac101306c04bc4b5492025aae2e6c102
teymour-aldridge <teymour.aldridge@icloud.com>
2020-09-28 10:02:42 +0100

    Initial commit

---
+++ js/src/main.js
@@ -0,0 +1,15 @@
+// display navbar
+
+let overlayActive = false;
+let toggleFunction = (e) => {
+    console.log("toggle")
+    e.preventDefault();
+    if (!overlayActive) {
+        document.getElementById("nav-overlay").style.display = "block";
+    } else {
+        document.getElementById("nav-overlay").style.display = "none";
+    }
+    overlayActive = !overlayActive;
+};
+document.getElementById("nav-icon").addEventListener("click", toggleFunction);
+document.getElementById("close-nav").addEventListener("click", toggleFunction);
